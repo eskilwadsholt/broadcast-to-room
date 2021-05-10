ec2b481011bbe1a7463fe3dffdf3a272861e4ce3
Eskil Simon Kanne Wadsholt <eskilsimonkannewadsholt@gmail.com>
2021-05-10 12:45:52 +0200

    build simple input form for chat-client

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,61 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="topbar">
+		<div class="field">
+			<label for="room">ROOM</label>
+			<input type="text" placeholder="Enter room PIN ...">
+		</div>
+		<div class="field">
+			<label for="user">USER</label>
+			<input type="text" placeholder="Enter name ...">
+		</div>
+	</div>
+
+	<div class="bottombar">
+		<input type="text" placeholder="Enter message ..." id="message">
+		<div class="quad"></div>
+		<button>SEND</button>
+	</div>
 </main>
 
 <style>
+	.bottombar, .topbar {
+		position: absolute;
+		display: flex;
+		align-items: center;
+		padding: 1em;
+		width: 100%;
+	}
+	.bottombar {
+		bottom: 0;
+	}
+	.topbar {
+		top: 0;
+		justify-content: space-around;
+	}
+	.quad {
+		width: 5px;
+	}
+	#message {
+		flex-grow: 1;
+	}
+	button {
+		background: #AAC;
+		color: white;
+		font-weight: 600;
+	}
+	label {
+		margin: 10px;
+	}
+	.field {
+		display: flex;
+	}
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		width: 100%;
+		height: 100%;
+		background: #5555;
 	}
 </style>
